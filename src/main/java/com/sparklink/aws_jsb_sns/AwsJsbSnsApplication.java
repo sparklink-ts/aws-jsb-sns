3eef941e75b1190335330daee424e9d8e47a5404
sparklink-ts <sparklink.ts@gmail.com>
2026-01-28 14:51:23 +0530

    base commit

---
+++ src/main/java/com/sparklink/aws_jsb_sns/AwsJsbSnsApplication.java
@@ -0,0 +1,13 @@
+package com.sparklink.aws_jsb_sns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsJsbSnsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsJsbSnsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparklink/aws_jsb_sns/AwsJsbSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparklink.aws_jsb_sns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsJsbSnsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
